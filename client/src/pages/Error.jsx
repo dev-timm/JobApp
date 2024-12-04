45c2fb22069ffd94a2bccdeacee70eb609b096ac
dev-timm <timm.kuemmeth@gmail.com>
2024-12-04 13:19:52 +0100

    add errorElement to HomeLayout

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,4 +1,14 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error Page</h1>;
+  const error = useRouteError();
+  console.log(error);
+
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <Link to="/">Back home</Link>
+    </div>
+  );
 };
 export default Error;
