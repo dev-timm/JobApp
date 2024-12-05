f7b1f74a29bec064b397314ec6d7b46f7a8f0b74
dev-timm <timm.kuemmeth@gmail.com>
2024-12-05 13:42:46 +0100

    add and style 404 page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,13 +1,30 @@
 import { Link, useRouteError } from 'react-router-dom';
 
+import styles from '../styles/Error.module.css';
+import img from '../assets/not-found.svg';
+
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
 
+  if (error.status === 404) {
+    return (
+      <div className={`${styles.page} container`}>
+        <img src={img} alt="not found" />
+        <h2>Lost in Space!</h2>
+        <p>
+          Oops! It looks like you've drifted off course. Our friendly alien is
+          beaming up the spaceship to help you find your way back. Let’s head
+          home and try again!
+        </p>
+        <Link to="/dashboard" className="btn-primary">
+          Return Home
+        </Link>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back home</Link>
+      <h3>Something went wrong</h3>
     </div>
   );
 };
