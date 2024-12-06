8d664b84ed57282a06d7599903533f4b76b03bc5
dev-timm <timm.kuemmeth@gmail.com>
2024-12-06 22:58:12 +0100

    create and style smallSidebar and all links in separate utils folder

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,10 +1,56 @@
 import { useDashboardContext } from '../pages/DashboardLayout';
 import styles from '../styles/SmallSidebar.module.css';
 
-const SmallSidebar = () => {
-  const data = useDashboardContext();
-  console.log(data);
+import links from '../utils/links';
+import close from '../assets/close.svg';
+import Logo from './Logo';
+import { NavLink } from 'react-router-dom';
 
-  return <div className={styles.smallSidebar}>SmallSidebar</div>;
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
+  return (
+    <div
+      className={
+        showSidebar
+          ? `${styles.sidebarContainer} ${styles.showSidebar}`
+          : styles.sidebarContainer
+      }
+    >
+      <div className={styles.content}>
+        <button
+          type="button"
+          className={styles.closeBtn}
+          onClick={toggleSidebar}
+        >
+          <img src={close} alt="close button" />
+        </button>
+        <header>
+          <Logo />
+        </header>
+        <div className={styles.navLinks}>
+          {links.map((link) => {
+            const { text, path, icon } = link;
+            return (
+              <NavLink
+                to={path}
+                key={text}
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles.navLink} ${styles.active}`
+                    : styles.navLink
+                }
+                onClick={toggleSidebar}
+              >
+                <span className={styles.icon}>{icon}</span>
+                {text}
+              </NavLink>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default SmallSidebar;
