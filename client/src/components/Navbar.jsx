1cfb860cc8c74fb0a00806df5ea6bb64da3ab251
dev-timm <timm.kuemmeth@gmail.com>
2024-12-06 16:20:26 +0100

    style navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,25 @@
+import { useDashboardContext } from '../pages/DashboardLayout';
+import styles from '../styles/Navbar.module.css';
+
+import menu from '../assets/menu.svg';
+import Logo from './Logo';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { toggleSidebar } = useDashboardContext();
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <button
+          type="button"
+          className={styles.toggleBtn}
+          onClick={toggleSidebar}
+        >
+          <img src={menu} alt="sidebar button" />
+        </button>
+        <Logo />
+        <div className={styles.btnContainer}>toggle/logout</div>
+      </div>
+    </div>
+  );
 };
 export default Navbar;
