1b5d8ed2f93d0f8115f3b80db377f4d52f13dc10
dev-timm <timm.kuemmeth@gmail.com>
2024-12-08 22:28:14 +0100

    connect server to db

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 const app = express();
 import morgan from 'morgan';
-import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 // routers
 import jobRouter from './routers/jobRouter.js';
@@ -41,6 +42,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
