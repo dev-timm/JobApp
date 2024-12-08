5c6a6113aad74c34c849e8342084d0396fa27f4b
dev-timm <timm.kuemmeth@gmail.com>
2024-12-08 14:25:03 +0100

    set up env file and add morgan package

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import express from 'express';
 const app = express();
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// makes sure morgan only runs in development not in production
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// shows helpful console logs
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -13,6 +24,7 @@ app.post('/', (req, res) => {
   res.json({ message: 'Data received', data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log('server running....');
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
 });
