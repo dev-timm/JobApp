31c8a777168cb0488280143752982283dba8d65f
dev-timm <timm.kuemmeth@gmail.com>
2024-12-09 14:08:18 +0100

    add updateJob controller

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,5 @@
 import Job from '../models/JobModel.js';
 
-import { nanoid } from 'nanoid';
-
-let jobs = [
-  { id: nanoid(), company: 'apple', position: 'front-end developer' },
-  { id: nanoid(), company: 'google', position: 'back-end developer' },
-];
-
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
   res.status(200).json({ jobs });
@@ -27,19 +20,17 @@ export const getJob = async (req, res) => {
 };
 
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+
+  if (!updatedJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
+  res.status(200).json({ job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
