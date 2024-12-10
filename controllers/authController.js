33576d1ddc6c7a21d235c508e67c5cb45ab40662
dev-timm <timm.kuemmeth@gmail.com>
2024-12-10 13:59:25 +0100

    add http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import { hashPassword, comparePassword } from '../utils/passwordUtils.js';
 import { UnauthenticatedError } from '../errors/customErrors.js';
+import { createJWT } from '../utils/tokenUtils.js';
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -21,5 +22,17 @@ export const login = async (req, res) => {
     user && (await comparePassword(req.body.password, user.password));
 
   if (!isValidUser) throw new UnauthenticatedError('invalid credentials');
-  res.send('login');
+
+  const token = createJWT({ userId: user._id, role: user.role });
+  console.log(token);
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  });
+
+  res.status(StatusCodes.CREATED).json({ msg: 'user logged in' });
 };
