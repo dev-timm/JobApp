244fc293509581065d763747a181473a973d8e07
dev-timm <timm.kuemmeth@gmail.com>
2024-12-12 14:01:55 +0100

    add functionality to login user on frontend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import { Form, Link } from 'react-router-dom';
+import { Link, Form, redirect, useNavigation } from 'react-router-dom';
 import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
 
 import styles from '../styles/RegisterAndLogin.module.css';
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post('/auth/login', data);
+    toast.success('Login successful');
+    return redirect('/dashboard');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <>
       <nav>
@@ -23,8 +41,12 @@ const Login = () => {
           placeholder="Enter pasword"
           defaultValue="secret123"
         />
-        <button type="submit" className="btn-medium-primary btn-form">
-          Sign In
+        <button
+          type="submit"
+          className="btn-medium-primary btn-form"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Signin in...' : 'Sign In'}
         </button>
         <p>
           Not a user yet?{' '}
