9cde0c368b48085d4874c1aac2a60713afa584f5
dev-timm <timm.kuemmeth@gmail.com>
2024-12-12 14:15:38 +0100

    set up loaders and get current user

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,15 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, redirect, useLoaderData } from 'react-router-dom';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
 
 import styles from '../styles/Dashboard.module.css';
 import { createContext, useContext, useState } from 'react';
 import { checkDefaultTheme } from '../App';
+import customFetch from '../utils/customFetch';
+
+// each route can define a "loader" function to provide data to the route element before it renders.
+export const loader = async () => {
+  try {
+    const { data } = await customFetch('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
+};
 
 const DashboardContext = createContext();
 
 const DashboardLayout = () => {
-  // temp
-  const user = { name: 'john' };
+  const { user } = useLoaderData();
+
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
 
@@ -46,7 +57,7 @@ const DashboardLayout = () => {
         <div>
           <Navbar />
           <div className={styles.dashboardPage}>
-            <Outlet />
+            <Outlet context={{ user }} />
           </div>
         </div>
       </main>
